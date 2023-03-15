7111ce28dc72b896f019c9b751fd362dfb2df5cf
Pedro Ingro <56962442+HawkEyeB@users.noreply.github.com>
2023-03-15 13:49:05 -0300

    Add files via upload

---
+++ ChapterFS11/ChapterFS11/Contexts/SqlContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ChapterFS11.Models;
+using Microsoft.Identity.Client;
+
+namespace ChapterFS11.Contexts
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() {}
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer("Data Source = DESKTOP-LANT3M9\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+                //optionsBuilder.UseSqlServer("Data Source = DESKTOP-LANT3M9\\SQLEXPRESS; initial catalog = Chapter; user id = sa; password = Espanha1*x");
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-LANT3M9\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true; TrustServerCertificate = True");
+
+            }
+        }
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
